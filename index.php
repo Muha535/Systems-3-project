fbe75f1e818735890d0cc4a110408e2411e5a2bc
Muhiddin Jusufagic <muhidin_22@hotmail.com>
2022-08-28 01:52:59 +0200

    Made the main page

--- index.php
+++ index.php
@@ -1,2 +1,71 @@
-<?php>
-echo hello world 
+
+
+<a href="login.php"> Login </a>
+<a href="Register.php"> Register </a>
+<a href="Fundraiser.php"> Donate</a>
+<a href="Profile.php"> Profile </a>
+<a href="Forum.php"> Forum</a>
+<style>
+    a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+</style>
+<style>
+body { 
+  background: url(Panda2.jpg) no-repeat center center fixed;
+  background-size: cover;
+}
+
+</style>
+<body background="Panda2.jpg">
+ 
+</body>
+
+<div class="header">
+  <h1>Endangered species Information system</h1>
+</div> 
+
+<style>
+.header {
+  padding: 60px;
+  text-align: center;
+  background: #1abc9c;
+  color: white;
+  font-size: 30px;
+}
+</style>
+
+<style>
+.container {  
+    top: 50%;
+        left: 50%;
+        width: 22em;
+        height: 12em;
+        margin-top: -2em;
+        margin-left: -8em;
+        position: fixed;  
+}  
+
+a.center {
+    background-color: #f44336 ;  
+color: White;  
+width: 110px;
+        height: 50px;
+        font-size:35px;
+        text-align:center; 
+        border-radius:15px;
+}
+</style>
+
+<div class="container">
+  <div class="center">
+  <a class="center" href="Browse.php"> Browse</a>
+  </div>
+</div>
+
+
